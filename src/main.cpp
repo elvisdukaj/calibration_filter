26015ff01f8a980f3d6a75f22f0b20a5449671a3
Elvis Dukaj <elvis.dukaj@gmail.com>
2017-06-13 23:28:11 +0200

    now I manage also 32 bit

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,14 @@
 #include "thresholdfilter.h"
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QQmlEngine>
+
 int main(int argc, char *argv[])
 {
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     QGuiApplication app(argc, argv);
-
-    qmlRegisterType<ThresholdFilter>("qubicaamf.vision", 1, 0, "ThresholdFilter");
+	
+	qmlRegisterType<ThresholdFilter>("qubicaamf.vision", 1, 0, "ThresholdFilter");
 
     QQmlApplicationEngine engine;
     engine.load(QUrl(QLatin1String("qrc:/main.qml")));
--- src/thresholdfilter.cpp
+++ src/thresholdfilter.cpp
@@ -1,5 +1,5 @@
 #include "thresholdfilter.h"
-#include <QPixmap>
+#include <QString>
 #include <QDebug>
 #include <stdexcept>
 #include <opencv2/imgproc.hpp>
@@ -22,30 +22,124 @@ QVideoFrame ThresholdFilterRunnable::run(QVideoFrame* frame, const QVideoSurface
 
     if (!isFrameValid(frame))
     {
-        qDebug() << "frame not valid";
-        return *frame;
+        qDebug() << "Frame is NOT valid";
+        return QVideoFrame{};
     }
 
-    QVideoFrame newFrame = *frame;
-    auto width = newFrame.width();
-    auto height = newFrame.height();
+#if 0
+    switch (frame->handleType()) {
+    case QAbstractVideoBuffer::NoHandle: qDebug() << "Handle Type: NoHandle"; break;
+    case QAbstractVideoBuffer::GLTextureHandle: qDebug() << "Handle Type: GLTextureHandle"; break;
+    case QAbstractVideoBuffer::XvShmImageHandle: qDebug() << "Handle Type: XvShmImageHandle"; break;
+    case QAbstractVideoBuffer::CoreImageHandle: qDebug() << "Handle Type: NoHandle"; break;
+    case QAbstractVideoBuffer::QPixmapHandle: qDebug() << "Handle Type: QPixmapHandle"; break;
+    case QAbstractVideoBuffer::EGLImageHandle: qDebug() << "Handle Type: EGLImageHandle"; break;
+    case QAbstractVideoBuffer::UserHandle: qDebug() << "Handle Type: UserHandle"; break;
+    }
 
-    newFrame.map(QAbstractVideoBuffer::ReadOnly);
-    auto data = newFrame.bits();
-    std::fill(data + (width * height), data + newFrame.mappedBytes(), 127);
-    cv::Mat grayscale(height, width, CV_8UC1, data);
-    cv::threshold(grayscale, grayscale, m_filter->threshold(), 255.0, cv::THRESH_BINARY);
+    switch (frame->pixelFormat()) {
+    case QVideoFrame::Format_Invalid: qDebug() << "Pixel Format: Format_Invalid"; break;
+    case QVideoFrame::Format_ARGB32: qDebug() << "Pixel Format: Format_ARGB32"; break;
+    case QVideoFrame::Format_ARGB32_Premultiplied: qDebug() << "Pixel Format: Format_ARGB32_Premultiplied"; break;
+    case QVideoFrame::Format_RGB32: qDebug() << "Pixel Format: Format_RGB32"; break;
+    case QVideoFrame::Format_RGB24: qDebug() << "Pixel Format: Format_RGB24"; break;
+    case QVideoFrame::Format_RGB565: qDebug() << "Pixel Format: Format_RGB565"; break;
+    case QVideoFrame::Format_RGB555: qDebug() << "Pixel Format: Format_RGB555"; break;
+    case QVideoFrame::Format_ARGB8565_Premultiplied: qDebug() << "Pixel Format: Format_ARGB8565_Premultiplied"; break;
+    case QVideoFrame::Format_BGRA32: qDebug() << "Pixel Format: Format_BGRA32"; break;
+    case QVideoFrame::Format_BGRA32_Premultiplied: qDebug() << "Pixel Format: Format_BGRA32_Premultiplied"; break;
+    case QVideoFrame::Format_BGR32: qDebug() << "Pixel Format: Format_BGR32"; break;
+    case QVideoFrame::Format_BGR24: qDebug() << "Pixel Format: Format_BGR24"; break;
+    case QVideoFrame::Format_BGR565: qDebug() << "Pixel Format: Format_BGR565"; break;
+    case QVideoFrame::Format_BGR555: qDebug() << "Pixel Format: Format_BGR555"; break;
+    case QVideoFrame::Format_BGRA5658_Premultiplied: qDebug() << "Pixel Format: Format_BGRA5658_Premultiplied"; break;
+    case QVideoFrame::Format_AYUV444_Premultiplied: qDebug() << "Pixel Format: Format_AYUV444_Premultiplied"; break;
+    case QVideoFrame::Format_YUV444: qDebug() << "Pixel Format: Format_YUV444"; break;
+    case QVideoFrame::Format_YUV420P: qDebug() << "Pixel Format: Format_YUV420P"; break;
+    case QVideoFrame::Format_YV12: qDebug() << "Pixel Format: Format_YV12"; break;
+    case QVideoFrame::Format_UYVY: qDebug() << "Pixel Format: Format_UYVY"; break;
+    case QVideoFrame::Format_YUYV: qDebug() << "Pixel Format: Format_YUYV"; break;
+    case QVideoFrame::Format_NV12: qDebug() << "Pixel Format: Format_NV12"; break;
+    case QVideoFrame::Format_NV21: qDebug() << "Pixel Format: Format_NV21"; break;
+    case QVideoFrame::Format_IMC1: qDebug() << "Pixel Format: Format_IMC1"; break;
+    case QVideoFrame::Format_IMC2: qDebug() << "Pixel Format: Format_IMC2"; break;
+    case QVideoFrame::Format_IMC3: qDebug() << "Pixel Format: Format_IMC3"; break;
+    case QVideoFrame::Format_IMC4: qDebug() << "Pixel Format: Format_IMC4"; break;
+    case QVideoFrame::Format_Y8: qDebug() << "Pixel Format: Format_Y8"; break;
+    case QVideoFrame::Format_Y16: qDebug() << "Pixel Format: Format_Y16"; break;
+    case QVideoFrame::Format_Jpeg: qDebug() << "Pixel Format: Format_Jpeg"; break;
+    case QVideoFrame::Format_CameraRaw: qDebug() << "Pixel Format: Format_CameraRaw"; break;
+    case QVideoFrame::Format_AdobeDng: qDebug() << "Pixel Format: Format_AdobeDng"; break;
+    }
+#endif
 
-    if (!newFrame.isValid())
-        qDebug() << "new frame is not valid";
+    auto width = frame->width();
+    auto height = frame->height();
 
-    newFrame.unmap();
+    cv::Mat mat;
 
-    return newFrame;
+    frame->map(QAbstractVideoBuffer::ReadOnly);
+
+    switch (frame->pixelFormat()) {
+    case QVideoFrame::Format_YUV420P:
+        mat = fromYUV420p(*frame);
+        break;
+
+    case QVideoFrame::Format_RGB32:
+        mat = fromRGB32(*frame);
+        break;
+
+    default:
+        qDebug() << "Format not supported";
+    }
+
+    frame->unmap();
+
+    cv::Mat grayScale;
+    cv::cvtColor(mat, grayScale, cv::COLOR_RGB2GRAY);
+
+    cv::threshold(grayScale, grayScale, m_filter->threshold(), 255.0, cv::THRESH_BINARY);
+
+    cv::cvtColor(grayScale, mat, cv::COLOR_GRAY2RGB);
+
+    QImage image(mat.ptr<uchar>(), width, height, QImage::Format_RGB888);
+
+//    static int i = 0;
+//    auto filename = std::string{"test"} + std::to_string(i++) + std::string{".bmp"};
+//    auto filename2 = std::string{"zzz"} + std::to_string(i++) + std::string{".bmp"};
+//    image.save(filename.c_str());
+//    cv::imwrite(filename2, mat);
+
+    return QVideoFrame{image};
 }
 
 bool ThresholdFilterRunnable::isFrameValid(QVideoFrame* frame) const noexcept
 {
-    return frame->isValid() && frame->handleType() == QAbstractVideoBuffer::NoHandle
-            && frame->pixelFormat() == QVideoFrame::Format_YUV420P;
+    return frame->isValid() && frame->handleType() == QAbstractVideoBuffer::NoHandle;
+}
+
+cv::Mat ThresholdFilterRunnable::fromYUV420p(QVideoFrame& frame) const
+{
+    auto data = frame.bits();
+    auto width = frame.width();
+    auto height = frame.height();
+
+    cv::Mat grayscale(height, width, CV_8UC1, data);
+    cv::Mat rgb;
+    cv::cvtColor(grayscale, rgb, cv::COLOR_GRAY2RGB);
+
+    return rgb;
+}
+
+cv::Mat ThresholdFilterRunnable::fromRGB32(QVideoFrame& frame) const
+{
+    auto data = frame.bits();
+    auto width = frame.width();
+    auto height = frame.height();
+
+    cv::Mat rgba(height, width, CV_8UC4, data);
+    cv::Mat rgb;
+    cv::cvtColor(rgba, rgb, cv::COLOR_RGBA2RGB);
+
+    return rgb;
 }
