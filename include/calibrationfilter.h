260724a9f8918cb8f28f40f4e6eea8a20c736ab4
Elvis Dukaj <elvis.dukaj@gmail.com>
2017-06-16 23:33:43 +0200

    improved calibration

--- include/calibrationfilter.h
+++ include/calibrationfilter.h
@@ -7,27 +7,32 @@
 class CalibrationFilter : public QAbstractVideoFilter {
     Q_OBJECT
 
+    Q_PROPERTY(int chessBoardWidth READ chessBoardWidth WRITE chessBoardWidth)
+    Q_PROPERTY(int chessBoardHeight READ chessBoardHeight WRITE chessBoardHeight)
+
     Q_PROPERTY(int threshold READ threshold WRITE threshold NOTIFY thresholdChanged)
+
     Q_PROPERTY(bool chessBoardFound READ isChessBoardFound WRITE setChessBoardFound NOTIFY chessBoardFound)
     Q_PROPERTY(bool showNegative READ showNegative WRITE setShowNegative)
-    Q_PROPERTY(bool calibrate READ isCalibrated WRITE calibrate NOTIFY calibrationFinished)
-    Q_PROPERTY(QImage unwraped READ getUnrwappedImage WRITE setUnrwappedImage    )
-
+    Q_PROPERTY(bool setCalibrated READ isCalibrated WRITE setCalibrated NOTIFY calibrationFinished)
 
 public:
     QVideoFilterRunnable* createFilterRunnable() override;
+
+    int chessBoardWidth() const noexcept{ return m_chessBoardWidth; }
+    void chessBoardWidth(int width) noexcept { m_chessBoardWidth = width; }
+
+    int chessBoardHeight() const noexcept { return m_chessBoardHeight; }
+    void chessBoardHeight(int height) noexcept { m_chessBoardHeight = height; }
 
     int threshold() const { return m_threshold; }
     void threshold(int thr) { m_threshold = thr; }
 
     int isCalibrated() const { return m_calibrated; }
-    void calibrate(bool cal) { m_calibrated = cal; }
+    void setCalibrated(bool cal = true) { m_calibrated = cal; }
 
     bool isChessBoardFound() const { return m_chessboardFound; }
     void setChessBoardFound(bool f) { m_chessboardFound = f; }
-
-    void setUnrwappedImage(QImage image) { m_unwrappedImage = image.copy(); }
-    QImage getUnrwappedImage() const { return m_unwrappedImage; }
 
     bool showNegative() const noexcept { return m_showNegative; }
     void setShowNegative(bool s) noexcept { m_showNegative = s; }
@@ -42,53 +47,46 @@ private:
 
 private:
     int m_threshold = 128;
+
+    int m_chessBoardWidth = 11;
+    int m_chessBoardHeight = 9;
+
     bool m_calibrated = false;
     bool m_cornerFound = false;
     bool m_chessboardFound = false;
     bool m_showNegative = false;
-    QImage m_unwrappedImage;
+
+    int m_goodFrames = 0;
+
 };
-
-
 
 class CameraCalibrator {
 public:
-    CameraCalibrator();
+    CameraCalibrator(const cv::Size boardSize = cv::Size{9, 6})
+        : m_boardSize{boardSize} {}
 
-    void processFrame(cv::Mat& grayscale);
-
-private:
-    std::vector<cv::Point2f> findChessboard(const cv::Mat& mat) const noexcept;
-
-    // Open chessboard images and extract corner points
-    bool addChessboardPoints(const cv::Mat& grayscale, const std::vector<cv::Point2f>& corners );
-    bool addChessboardPoints(const std::vector<std::string>& filelist, cv::Size& boardSize);
-
-    // Add scene points and corresponding image points
-    void addPoints(const std::vector<cv::Point2f>& imageCorners, const std::vector<cv::Point3f>& objectCorners);
-
-    // Calibrate the camera
-    // returns the re-projection error
+    std::vector<cv::Point2f> findChessboard(const cv::Mat& mat);
     double calibrate(cv::Size& imageSize);
-
-    // remove distortion in an image (after calibration)
     cv::Mat remap(const cv::Mat& image);
+    const cv::Size boardSize() const noexcept { return m_boardSize; }
 
 private:
-    // input points:
-    // the points in world coordinates
-    std::vector<std::vector<cv::Point3f>> m_objectPoints;
-    // the point positions in pixels
+    void addPoints(const std::vector<cv::Point2f>& imageCorners);
+
+private:
+    cv::Size m_boardSize;
+
+    std::vector<std::vector<cv::Point3f>> m_worldObjectPoints;
     std::vector<std::vector<cv::Point2f>> m_imagePoints;
-    // output Matrices
+
+    cv::Mat m_foundChessBoard;
+
     cv::Mat m_cameraMatrix;
     cv::Mat m_distCoeffs;
-    // flag to specify how calibration is done
-    int m_flag;
-    // used in image undistortion
-    cv::Mat m_map1,m_map2;
+    std::vector<cv::Mat> m_rotationVecs;
+    std::vector<cv::Mat> m_translationtVecs;
+    cv::Mat m_mapX, m_mapY;
     bool m_mustInitUndistort;
-
 };
 
 class CalibrationFilterRunnable : public AbstractVideoFilterRunnable {
@@ -105,4 +103,7 @@ private:
     std::vector<cv::Point2d> m_imagePoints;
     cv::Mat m_lastFrameWithChessBoard;
     bool m_finished = false;
+    CameraCalibrator m_calibrator;
+    int m_goodFrames = 0;
+    cv::Mat m_unwrapped;
 };
--- src/calibrationfilter.cpp
+++ src/calibrationfilter.cpp
@@ -36,31 +36,61 @@ QVideoFrame CalibrationFilterRunnable::run(QVideoFrame* frame, const QVideoSurfa
 
         if (m_filter->showNegative())
         {
-            cv::Mat frameMat;
             convertToCvMat(frame, frameMat);
-            m_lastFrameWithChessBoard.copyTo(frameMat);
+            cv::flip(frameMat, frameMat, 1);
+            m_lastFrameWithChessBoard.copyTo(
+                        frameMat(cv::Rect{
+                                     0, 0,
+                                     m_lastFrameWithChessBoard.cols,
+                                     m_lastFrameWithChessBoard.rows
+                                 }
+                                 )
+                        );
+        }
+        else if(m_filter->isCalibrated())
+        {
+            convertToCvMat(frame, frameMat);
+            m_unwrapped.copyTo(frameMat);
         }
         else
         {
             videoframeToGrayscale(frame, grayscale, frameMat);
 
             cv::flip(grayscale, grayscale, 1);
-            cv::threshold(grayscale, grayscale, m_filter->threshold(), 255.0, cv::THRESH_BINARY);
 
-            vector<cv::Point2f> corners;
-            auto res = cv::findChessboardCorners(grayscale, cv::Size(9, 6), corners);
+            auto corners = m_calibrator.findChessboard(grayscale);
 
             grayscaleToVideoFrame(frame, grayscale, frameMat);
 
-            if (res)
+            if (!corners.empty())
             {
-                cv::cornerSubPix(grayscale, corners, cv::Size(11, 11), cv::Size(-1, -1),
-                                 cv::TermCriteria(CV_TERMCRIT_EPS + CV_TERMCRIT_ITER, 30, 0.1));
+                cv::drawChessboardCorners(
+                            frameMat,
+                            m_calibrator.boardSize(),
+                            corners,
+                            true
+                            );
 
-                cv::drawChessboardCorners(frameMat, cv::Size(9, 6), cv::Mat(corners), res);
-                m_lastFrameWithChessBoard = frameMat.clone();
-                m_filter->setChessBoardFound(true);
                 emit m_filter->chessBoardFound();
+                cv::bitwise_not(frameMat, m_lastFrameWithChessBoard);
+                cv::resize(m_lastFrameWithChessBoard, m_lastFrameWithChessBoard,
+                           cv::Size{0, 0}, 0.3, 0.3
+                           );
+
+                ++m_goodFrames;
+                qDebug() << "Frame number " << m_goodFrames;
+
+                if (m_goodFrames == 15)
+                {
+                    auto error = m_calibrator.calibrate(grayscale.size());
+                    qDebug() << "calibration done! Error: " << error;
+
+                    m_unwrapped = m_calibrator.remap(frameMat);
+                    m_unwrapped.copyTo(frameMat);
+
+                    m_filter->setCalibrated();
+                    emit m_filter->calibrationFinished();
+                }
             }
         }
     }
@@ -97,102 +127,55 @@ void CalibrationFilterRunnable::convertToCvMat(QVideoFrame *frame, cv::Mat& fram
     }
 }
 
-CameraCalibrator::CameraCalibrator()
-    : m_flag(0), m_mustInitUndistort(true)
+vector<cv::Point2f> CameraCalibrator::findChessboard(const cv::Mat& grayscale)
 {
-}
-
-void CameraCalibrator::processFrame(cv::Mat& grayscale)
-{
-    auto corners = findChessboard(grayscale);
-
-    if (!corners.empty())
+    vector<cv::Point2f> imageCorners;
+    if (cv::findChessboardCorners(grayscale, m_boardSize, imageCorners, cv::CALIB_CB_FAST_CHECK))
     {
+        cv::cornerSubPix(
+                    grayscale, imageCorners, m_boardSize,
+                    cv::Size(-1, -1),
+                    cv::TermCriteria(
+                        CV_TERMCRIT_EPS + CV_TERMCRIT_ITER,
+                        300, 0.01)
+                    );
 
-        addChessboardPoints(grayscale, corners)
+        if (imageCorners.size() == m_boardSize.area())
+            addPoints(imageCorners);
+        else
+            imageCorners.clear();
     }
+
+    return imageCorners;
 }
 
-vector<cv::Point2f> CameraCalibrator::findChessboard(const cv::Mat &mat) const noexcept
+void CameraCalibrator::addPoints(const std::vector<cv::Point2f> &imageCorners)
 {
-    vector<cv::Point2f> corners;
-    cv::findChessboardCorners(mat, cv::Size(9, 6), corners);
-    return corners;
-}
+    vector<cv::Point3f> objectCorners;
 
-bool CameraCalibrator::addChessboardPoints(const cv::Mat& grayscale, const std::vector<cv::Point2f>& corners)
-{
-    cv::cornerSubPix(grayscale, corners, cv::Size(11, 11), cv::Size(-1, -1),
-                     cv::TermCriteria(CV_TERMCRIT_EPS + CV_TERMCRIT_ITER, 30, 0.1));
-}
+    for (auto y = 0; y < m_boardSize.height; ++y)
+        for (auto x = 0; x < m_boardSize.width; ++x)
+            objectCorners.push_back(cv::Point3f(y, x, 0.0f));
 
-int CameraCalibrator::addChessboardPoints(const std::vector<string>& filelist, cv::Size& boardSize)
-{
-    // the points on the chessboard
-    std::vector<cv::Point2f> imageCorners;
-    std::vector<cv::Point3f> objectCorners;
-    // 3D Scene Points:
-    // Initialize the chessboard corners
-    // in the chessboard reference frame
-    // The corners are at 3D location (X,Y,Z)= (i,j,0)
-    for (int i=0; i<boardSize.height; i++) {
-        for (int j=0; j<boardSize.width; j++) {
-            objectCorners.push_back(cv::Point3f(i, j, 0.0f));
-        }
-    }
-    // 2D Image points:
-    cv::Mat image; // to contain chessboard image
-    int successes = 0;
-    // for all viewpoints
-    for (int i=0; i<filelist.size(); i++) {
-        // Open the image
-        image = cv::imread(filelist[i],0);
-        // Get the chessboard corners
-        bool found = cv::findChessboardCorners(
-                         image, boardSize, imageCorners);
-        // Get subpixel accuracy on the corners
-        cv::cornerSubPix(image, imageCorners,
-                         cv::Size(5,5),
-                         cv::Size(-1,-1),
-                         cv::TermCriteria(cv::TermCriteria::MAX_ITER +
-                                          cv::TermCriteria::EPS,
-                                          30, // max number of iterations
-                                          0.1)); // min accuracy
-        //If we have a good board, add it to our data
-        if (imageCorners.size() == boardSize.area()) {
-            // Add image and scene points from one view
-            addPoints(imageCorners, objectCorners);
-            successes++;
-        }
-    }
-    return successes;
-}
-
-void CameraCalibrator::CameraCalibrator::addPoints(
-        const std::vector<cv::Point2f>& imageCorners,
-        const std::vector<cv::Point3f> &objectCorners)
-{
-    // 2D image points from one view
+    m_worldObjectPoints.push_back(objectCorners);
     m_imagePoints.push_back(imageCorners);
-    // corresponding 3D scene points
-    m_objectPoints.push_back(objectCorners);
 }
+
 
 double CameraCalibrator::calibrate(cv::Size& imageSize)
 {
-    // undistorter must be reinitialized
-    m_mustInitUndistort= true;
-    //Output rotations and translations
-    std::vector<cv::Mat> rvecs, tvecs;
+    m_mustInitUndistort = true;
+
     // start calibration
-    return
-            calibrateCamera(m_objectPoints, // the 3D points
-                            m_imagePoints, // the image points
-                            imageSize, // image size
-                            m_cameraMatrix,// output camera matrix
-                            m_distCoeffs, // output distortion matrix
-                            rvecs, tvecs,// Rs, Ts
-                            m_flag); // set options
+    return cv::calibrateCamera(
+                m_worldObjectPoints,
+                m_imagePoints,
+                imageSize,
+                m_cameraMatrix,
+                m_distCoeffs,
+                m_rotationVecs,
+                m_translationtVecs
+                );
 }
 
 cv::Mat CameraCalibrator::CameraCalibrator::remap(const cv::Mat& image)
@@ -201,16 +184,87 @@ cv::Mat CameraCalibrator::CameraCalibrator::remap(const cv::Mat& image)
     if (m_mustInitUndistort) { // called once per calibration
         cv::initUndistortRectifyMap(
                     m_cameraMatrix, // computed camera matrix
-                    m_distCoeffs, // computed distortion matrix
-                    cv::Mat(), // optional rectification (none)
-                    cv::Mat(), // camera matrix to generate undistorted
-                    image.size(), // size of undistorted
-                    CV_32FC1, // type of output map
-                    m_map1, m_map2); // the x and y mapping functions
+                    m_distCoeffs,   // computed distortion matrix
+                    cv::Mat(),      // optional rectification (none)
+                    cv::Mat(),      // camera matrix to generate undistorted
+                    image.size(),   // size of undistorted
+                    CV_32FC1,       // type of output map
+                    m_mapX, m_mapY  // the x and y mapping functions
+                    );
         m_mustInitUndistort= false;
     }
+
     // Apply mapping functions
-    cv::remap(image, undistorted, m_map1, m_map2,
-              cv::INTER_LINEAR); // interpolation type
+    cv::remap(image, undistorted, m_mapX, m_mapY, cv::INTER_LINEAR);
     return undistorted;
 }
+
+
+//int CameraCalibrator::addChessboardPoints(const std::vector<std::string>& filelist,
+//                                          cv::Size & boardSize) {
+//    // the points on the chessboard
+//    std::vector<cv::Point2f> imageCorners;
+//    std::vector<cv::Point3f> objectCorners;
+//    // 3D Scene Points:
+//    // Initialize the chessboard corners
+//    // in the chessboard reference frame
+//    // The corners are at 3D location (X,Y,Z)= (i,j,0)
+//    for (int i=0; i<boardSize.height; i++) {
+//        for (int j=0; j<boardSize.width; j++) {
+//            objectCorners.push_back(cv::Point3f(i, j, 0.0f));
+//        }
+//    }
+//    // 2D Image points:
+//    cv::Mat image; // to contain chessboard image
+//    int successes = 0;
+//    // for all viewpoints
+//    for (int i=0; i<filelist.size(); i++) {
+//        // Open the image
+//        image = cv::imread(filelist[i],0);
+//        // Get the chessboard corners
+//        bool found = cv::findChessboardCorners(
+//                    image, boardSize, imageCorners);
+//        // Get subpixel accuracy on the corners
+//        cv::cornerSubPix(image, imageCorners,
+//                         cv::Size(5,5),
+//                         cv::Size(-1,-1),
+//                         cv::TermCriteria(cv::TermCriteria::MAX_ITER +
+//                                          cv::TermCriteria::EPS,
+//                                          30, // max number of iterations
+//                                          0.1)); // min accuracy
+//        //If we have a good board, add it to our data
+//        if (imageCorners.size() == boardSize.area()) {
+//            // Add image and scene points from one view
+//            addPoints(imageCorners, objectCorners);
+//            successes++;
+//        }
+//    }
+//    return successes;
+//}
+
+//void CameraCalibrator::CameraCalibrator::addPoints(
+//        const std::vector<cv::Point2f>& imageCorners,
+//        const std::vector<cv::Point3f> &objectCorners)
+//{
+//    // 2D image points from one view
+//    m_imagePoints.push_back(imageCorners);
+//    // corresponding 3D scene points
+//    m_objectPoints.push_back(objectCorners);
+//}
+
+//double CameraCalibrator::calibrate(cv::Size& imageSize)
+//{
+//    // undistorter must be reinitialized
+//    m_mustInitUndistort= true;
+//    //Output rotations and translations
+//    std::vector<cv::Mat> rvecs, tvecs;
+//    // start calibration
+//    return
+//            calibrateCamera(m_objectPoints, // the 3D points
+//                            m_imagePoints, // the image points
+//                            imageSize, // image size
+//                            m_cameraMatrix,// output camera matrix
+//                            m_distCoeffs, // output distortion matrix
+//                            rvecs, tvecs,// Rs, Ts
+//                            m_flag); // set options
+//}
